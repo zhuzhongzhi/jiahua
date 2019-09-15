cbd6fd7645404776b5882568bf6fc319e0f71384
Zhu Zhongzhi <zhongzhi.zhu@doforsys.com>
2019-09-15 23:04:24 +0800

    调测修改, 修改用户、权限模块

--- src/main/java/com/xgit/iot/service/craft/WagonExceptionService.java
+++ src/main/java/com/xgit/iot/service/craft/WagonExceptionService.java
@@ -1,22 +1,15 @@
 package com.xgit.iot.service.craft;
 
 import com.bkrwin.ufast.feign.GenClient;
-import com.bkrwin.ufast.infra.infra.PageCommonVO;
-import com.bkrwin.ufast.infra.infra.SearchCommonVO;
-import com.github.pagehelper.PageHelper;
-import com.github.pagehelper.PageInfo;
 import com.xgit.iot.dao.entity.craft.WagonExceptionDO;
 import com.xgit.iot.dao.entity.craft.WagonOperateDO;
-import com.xgit.iot.dao.entity.system.IusAccountDO;
 import com.xgit.iot.dao.mapper.craft.WagonExceptionMapper;
-import com.xgit.iot.dao.mapper.craft.WagonOperateMapper;
 import com.xgit.iot.service.BaseService;
 import com.xgit.iot.service.vo.craft.WagonExceptionVO;
 import com.xgit.iot.service.vo.craft.WagonOperateVO;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
